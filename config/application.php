d5ce9966e7a65ccd511492b39dbfb40a8582e184
Ioannis Tzavaras <ioannis-tzavaras@tips-matsers.com>
2015-01-28 16:18:46 +0100

    Application
    That is the application config file.

---
+++ config/application.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Tips Masters Version 1.0.0
+ * 
+ * That is a new football betting social network
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2015, Ioannis Tzavaras
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	Tips Masters Beta
+ * @author      Ioannis Tzavaras <ioammis-tzavaras@tips-masters.com>
+ * @copyright   (c) 2015, Ioannis Tzavaras
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://tips-masters.com
+ * @since       V 1.0.0
+ * @filesource
+ */
+
+/*
+ *---------------------------------------------------------------
+ * Application Name
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the name of the website.
+ */
+$config['app_name'] = "Tips Masters";
+
+/*
+ *---------------------------------------------------------------
+ * Application Version
+ *---------------------------------------------------------------
+ *
+ * This variable must contain the version of the website.
+ */
+$config['app_version'] = "1.0.0";
+
+/* End of file application.php */
+/* Location: ./config/application.php */
