6e9ba903a27da026576d1e849a519636a17660f8
Ioannis Tzavaras <ioannis-tzavaras@tips-matsers.com>
2015-01-28 19:38:14 +0100

    Autoload helpers
    autoload url and language helpers

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -88,7 +88,10 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+    'url',
+    'language'
+);
 
 
 /*
